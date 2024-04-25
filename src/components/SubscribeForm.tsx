973abb26bf5b306b8dc0b532b31eef01caa9694d
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-04-25 15:00:42 +0530

    complete

--- src/components/SubscribeForm.tsx
+++ src/components/SubscribeForm.tsx
@@ -6,30 +6,62 @@ const SubscribeForm = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const formRef = useRef<HTMLFormElement>(null);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submitting");
 
     if (isSubmitting) return;
     setIsSubmitting(true);
-
-    const subToast = toast.loading("Submitting...");
+    const subToast = toast.loading("Submitting…");
 
     const formData = new FormData(e.currentTarget);
     const formInputs = Object.fromEntries(formData);
-    // console.log("formInputs:", formInputs);
 
-    if (!formInputs?.email) {
-      return toast.error("Please enter an email address", {
+    const email = formInputs.email;
+
+    // email exists
+    if (!email) {
+      return toast.error("Please provide an email address", {
         id: subToast,
       });
     }
 
     // validate email
-    if (!validateEmail((formInputs.email as String).trim())) {
-      return toast.error("Please Provide a Valid email address", {
+    if (!validateEmail((email as string).trim())) {
+      return toast.error("Please provide a valid email address", {
         id: subToast,
       });
+    }
+
+    try {
+      const res = await fetch("/api/subscribe.json", {
+        method: "POST",
+        body: JSON.stringify(formInputs),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error("Yikes!");
+      }
+
+      const successMessage = await res.json();
+
+      toast.success(successMessage.message, {
+        id: subToast,
+      });
+
+      formRef.current?.reset();
+      setIsSubmitting(false);
+    } catch (e) {
+      setIsSubmitting(false);
+      toast.error("There was a problem subscribing you. Please try again.", {
+        id: subToast,
+      });
+      if (e instanceof Error) {
+        return console.error(e.message);
+      }
+      console.error(e);
     }
   };
 
@@ -39,9 +71,8 @@ const SubscribeForm = () => {
       className="grid gap-2 p-4 border-2"
       onSubmit={handleSubmit}
     >
-      <label htmlFor="email">Enter you Email</label>
-      <input type="email" name="email" required id="email" />
-
+      <label htmlFor="email">Enter your email</label>
+      <input type="email" name="email" id="email" required />
       <button type="submit" disabled={isSubmitting}>
         Submit
       </button>
@@ -49,5 +80,4 @@ const SubscribeForm = () => {
     </form>
   );
 };
-
 export default SubscribeForm;
